6a6c55a4c4df52ba0ed5b0fa0d990fc3445bc74a
The1Yu <1500968308@qq.com>
2020-11-19 22:46:47 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @program: git_workspace
+ * @description:
+ * @author: Mr.Zhang
+ * @create: 2020-11-19 22:35
+ **/
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
